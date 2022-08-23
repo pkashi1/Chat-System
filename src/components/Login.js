f140ef3999edae49dd0a6106c6604d902c22bc92
Mohammad Rasool <ummaduddin29@gmail.com>
2022-08-22 22:36:18 -0500

    Worked more on login page, background, buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,12 +3,20 @@ import React from 'react';
 //Importing Icons for Login Page
 import { FacebookOutlined, GoogleOutlined } from '@ant-design/icons';
 
-const Login = () =>{
+const Login = () => {
 
-    return(
+    return (
         <div id="login-page">
             <div id="login-card">
-                <h2>Welcome!</h2>
+                <h2 className='fading-text'>Welcome!</h2>
+                <div className='login-button google'>
+                    <GoogleOutlined/>{' '}{' '}Sign In With Google
+                </div>
+                <br></br>
+                <br></br>
+                <div className='login-button facebook'>
+                    <FacebookOutlined/>{' '}{' '}Sign In With Facebook
+                </div>
             </div>
         </div>
     );
