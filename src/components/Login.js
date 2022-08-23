06497deaebde6aa1ced01787d5547c53ec210cdd
Mohammad Rasool <ummaduddin29@gmail.com>
2022-08-22 20:52:53 -0500

    Started Login Page and added Welcome Page

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+//importing React
+import React from 'react';
+//Importing Icons for Login Page
+import { FacebookOutlined, GoogleOutlined } from '@ant-design/icons';
+
+const Login = () =>{
+
+    return(
+        <div id="login-page">
+            <div id="login-card">
+                <h2>Welcome!</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
