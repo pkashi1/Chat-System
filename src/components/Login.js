a5e0ee245c1d69d49b6f166474b02dce4df17a87
Parimal Kashireddy <pkashi1@lsu.edu>
2022-09-03 20:34:06 -0500

    added AuthContext.js file and some code to Login.js and tried to connect firebase, there is an error in line 17 and 24 inside Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
 //importing React
 import React from 'react';
-//Importing Icons for Login Page
+//Importing Icons for Login Page 
 import { FacebookOutlined, GoogleOutlined } from '@ant-design/icons';
+import "firebase/app";
+
+import { auth } from '..src/firebase.js';
 
 const Login = () => {
 
@@ -10,13 +13,16 @@ const Login = () => {
             <div id="login-card">
                 <h2>Welcome to <a href="https://github.com/pkashi1/Chat-System" title='GitHub Link'>Chit-Chat!</a></h2>
                 <p>A Simple, Secure, and User-Friendly Messenger</p>
-                <div className='login-button google'>
-                    <GoogleOutlined/>{' '}{' '}Sign In With Google
+                <div className="login-button google"
+                    onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+                    >
+                    <GoogleOutlined/>Sign In With Google
                 </div>
                 <br></br>
                 <br></br>
-                <div className='login-button facebook'>
-                    <FacebookOutlined/>{' '}{' '}Sign In With Facebook
+                <div className="login-button facebook"
+                    onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
+                    <FacebookOutlined/>Sign In With Facebook
                 </div>
             </div>
         </div>
