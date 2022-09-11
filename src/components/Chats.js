825ca60f22ec8f6ff0ad9ed82107cd1507046b7f
Parimal Kashireddy <pkashi1@lsu.edu>
2022-09-10 23:46:33 -0500

    Added the google API, Facebook API to be added

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import { auth } from '../firebase';
 
+import { useAuth } from '../contexts/AuthContext';
 const Chats = () => {
+    const history = useHistory();
+    const { user } = useAuth();
+    console.log(user);
+
+    const handleLogout = async () => {
+        await auth.signOut();
+
+        history.push('/');
+    }
     return (
-        <h1>Chats</h1>
+       <div className="chats-page">
+           <div className="nav-bar">
+               <div className="logo-tab">
+                    ChitChat
+               </div>
+               <div onClick={handleLogout} className="logout-tab">
+                    Logout
+               </div>
+           </div>
+           <ChatEngine height = "calc(100vh - 66px)"
+                projectId=""
+                userName="."
+                userSecret=".">
+
+           </ChatEngine>
+
+       </div>
     )
 } 
 
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
         auth.onAuthStateChanged((user) => {
             setUser(user);
             setLoading(false);
-            history.push('/chats');
+            if(user) history.push('/chats');
         })
     },[user,history]);
     const value = { user };
